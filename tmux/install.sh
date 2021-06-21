02b3e87ec07a9f5281d43e83f45e96d6c86fdbea
Pawel Lipinski <lipinskijob@gmail.com>
2021-06-21 22:21:15 +0200

    Echo required programs by nvim config

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,3 +6,5 @@ mkdir $config
 ln -sr .tmux.conf $config/.tmux.conf
 ln -sr tmux-redirect $config/tmux-redirect
 ln -s  $config/.tmux.conf ~/.tmux.conf
+
+echo "You have to installed xclip, fzf, ripgrep"
