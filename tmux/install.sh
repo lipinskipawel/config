1bf19887af93fbb33ebf4cf857a9a168c0c51e43
Pawel Lipinski <lipinskijob@gmail.com>
2021-06-21 22:31:06 +0200

    Use base16 theme in tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,6 +3,7 @@
 config=~/.config/tmux
 mkdir $config
 
+ln -sr theme.conf $config/theme.conf
 ln -sr .tmux.conf $config/.tmux.conf
 ln -sr tmux-redirect $config/tmux-redirect
 ln -s  $config/.tmux.conf ~/.tmux.conf
