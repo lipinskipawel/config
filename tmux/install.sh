aee9d0425eb4cfa6b5f98248818bed222ecd8cd5
Pawel Lipinski <lipinskijob@gmail.com>
2021-08-13 20:03:20 +0200

    Ask user to delete tmux config before installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
 config=~/.config/tmux
-mkdir $config
 
-ln -sr theme.conf $config/theme.conf
-ln -sr .tmux.conf $config/.tmux.conf
-ln -sr tmux-redirect $config/tmux-redirect
-ln -s  $config/.tmux.conf ~/.tmux.conf
+function install_tmux() {
+    echo "Installing..."
+    ln -sr theme.conf $config/theme.conf
+    ln -sr .tmux.conf $config/.tmux.conf
+    ln -sr tmux-redirect $config/tmux-redirect
+    ln -s $config/.tmux.conf ~/.tmux.conf
+}
 
-echo "You have to installed xclip, fzf, ripgrep"
+echo ""
+echo "Files to delete"
+echo $config
+echo "~/.tmux.conf"
+echo ""
+
+read -r -p "Do you want to remove existing tmux config? [y/N] " yn
+case $yn in
+    [Yy]*)
+    echo "You have to install following packages"
+    echo "             fzf"
+    echo "             ripgrep"
+    echo "             xclip"
+    echo ""
+    echo "Removing existing configuration"
+    rm -rf $config
+    rm -rf ~/.tmux.conf
+    mkdir $config
+    install_tmux;;
+    *)
+        echo "exit"
+        exit;;
+esac
+
+
