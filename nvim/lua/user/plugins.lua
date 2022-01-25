389eaca7948936a3a1dd67730aee3755a0cd0a71
Pawel Lipinski <lipinskijob@gmail.com>
2022-01-25 11:25:52 +0100

    Add autopair plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -76,6 +76,9 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
 
+  -- autopairs
+  use 'windwp/nvim-autopairs'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
