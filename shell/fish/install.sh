7974039bcd14736acc2fa6166f60085af2efe411
Pawel Lipinski <lipinskijob@gmail.com>
2022-04-18 16:59:31 +0200

    Use fish whenever alacritty and tmux is started

--- shell/fish/install.sh
+++ shell/fish/install.sh
@@ -2,5 +2,10 @@
 
 cd ..
 fish_conf_file=~/.config/fish/config.fish
+fish_conf_d=~/.config/fish/conf.d
+
 rm $fish_conf_file
+rm -rf $fish_conf_d
+
 ln -sr fish/config.fish $fish_conf_file
+ln -sr fish/conf.d $fish_conf_d
