5a84f2436c04ced7785e832a5df8b096d6038723
Pawel Lipinski <lipinskijob@gmail.com>
2022-04-27 12:36:19 +0200

    Add take function to fish config

--- shell/fish/install.sh
+++ shell/fish/install.sh
@@ -3,9 +3,12 @@
 cd ..
 fish_conf_file=~/.config/fish/config.fish
 fish_conf_d=~/.config/fish/conf.d
+fish_functions=~/.config/fish/functions
 
 rm $fish_conf_file
 rm -rf $fish_conf_d
+rm -rf $fish_functions
 
 ln -sr fish/config.fish $fish_conf_file
 ln -sr fish/conf.d $fish_conf_d
+ln -sr fish/functions $fish_functions
