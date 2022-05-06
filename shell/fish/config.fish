0ca516b589def54c537979123c96e60176f7dc2f
Pawel Lipinski <lipinskijob@gmail.com>
2022-05-06 08:18:44 +0200

    Add fish abbreviations

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,3 +1,9 @@
+abbr -a yr 'cal -y'
+abbr -a e nvim
+abbr -a g git
+abbr -a ga 'git commit --amend'
+abbr -a gc 'git checkout'
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
