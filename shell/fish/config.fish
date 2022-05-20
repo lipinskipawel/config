353642bbc8946078253360e44090f89a9ebdcb68
Pawel Lipinski <lipinskijob@gmail.com>
2022-05-20 15:47:30 +0200

    Add abbreviations for switching archlinux-java

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -3,6 +3,9 @@ abbr -a e nvim
 abbr -a g git
 abbr -a ga 'git commit --amend'
 abbr -a gc 'git checkout'
+abbr -a j8 'sudo archlinux-java set java-8-openjdk'
+abbr -a j11 'sudo archlinux-java set java-11-openjdk'
+abbr -a j17 'sudo archlinux-java set java-17-openjdk'
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
