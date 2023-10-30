e3aeac56ac29743daecad8218e1f7b53afce62fa
Pawel Lipinski <lipinskijob@gmail.com>
2023-10-30 07:09:01 +0100

    [nvim] Setup conform to format code

---
+++ nvim2/lua/me/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formatters_by_ft = {
+                lua = { "stylua" },
+                -- Conform will run multiple formatters sequentially
+                -- python = { "isort", "black" },
+                -- Use a sub-list to run only the first available formatter
+                -- javascript = { { "prettierd", "prettier" } },
+            },
+        })
+        -- Then you can use conform.format() just like you would
+        -- vim.lsp.buf.format(). For example, to format on save:
+        -- vim.api.nvim_create_autocmd("BufWritePre", {
+        --   pattern = "*",
+        --   callback = function(args)
+        --     require("conform").format({ bufnr = args.buf })
+        --   end,
+        -- })
+        -- or just setup this on the setup function using format_on_save.
+    end,
+}
