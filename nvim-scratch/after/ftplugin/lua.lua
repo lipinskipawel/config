fc721929b99d18fcedc2f8364fcf385b7271199b
Pawel Lipinski <lipinskijob@gmail.com>
2024-12-26 09:21:50 +0100

    [nvim-scratch] Add formating on save

--- nvim-scratch/after/ftplugin/lua.lua
+++ nvim-scratch/after/ftplugin/lua.lua
@@ -3,3 +3,6 @@ local set = vim.opt_local
 set.shiftwidth = 4
 set.number = true
 set.relativenumber = true
+
+set.tabstop = 4
+set.expandtab = true -- converts tabs into spaces
--- nvim-scratch/lua/config/plugins/lsp.lua
+++ nvim-scratch/lua/config/plugins/lsp.lua
@@ -2,19 +2,35 @@ return {
     'neovim/nvim-lspconfig',
     enabled = true,
     dependencies = {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	    library = {
-		-- See the configuration section for more details
-		-- Load luvit types when the `vim.uv` word is found
-		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	    },
-	},
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
     },
     config = function()
-	require("lspconfig").lua_ls.setup {
-	    cmd = { "lua_ls" },
-	}
+        require("lspconfig").lua_ls.setup {
+            cmd = { "lua_ls" },
+        }
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(args)
+                local client = vim.lsp.get_client_by_id(args.data.client_id)
+                if not client then return end
+
+                if client.supports_method('text/Document/formatting') then
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        buffer = args.buf,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                        end,
+                    })
+                end
+            end,
+        })
     end
 }
