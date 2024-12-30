f534f9647ecdc491bc7af6514798a3aaec0b3e7e
Pawel Lipinski <lipinskijob@gmail.com>
2024-12-30 11:53:31 +0100

    [nvim-scratch] Customise menu on right click

---
+++ nvim-scratch/plugin/menu.lua
@@ -0,0 +1,41 @@
+vim.cmd [[
+    aunmenu PopUp
+    anoremenu PopUp.Inspect         <cmd>Inspect<CR>
+    amenu PopUp.-1-                 <NOP>
+    anoremenu PopUp.Definition      <cmd>lua vim.lsp.buf.definition()<CR>
+    anoremenu PopUp.References      <cmd>lua vim.lsp.buf.references()<CR>
+    anoremenu PopUp.TReferences     <cmd>Telescope lsp_references<CR>
+    amenu PopUp.-2-                 <NOP>
+    anoremenu PopUp.Back            <C-t>
+    "anoremenu PopUp.URL             gx
+]]
+
+-- clear default behaviour
+local group = vim.api.nvim_create_augroup("nvim_popupmenu", { clear = true })
+
+vim.api.nvim_create_autocmd("MenuPopup", {
+    pattern = "*",
+    group = group,
+    desc = "Custom PopUp Setup",
+    callback = function()
+        vim.cmd [[
+            amenu disable PopUp.Definition
+            amenu disable PopUp.References
+            amenu disable PopUp.TReferences
+            "amenu disable PopUp.URL
+        ]]
+        if vim.lsp.get_clients({ bufnr = 0 })[1] then
+            vim.cmd [[
+            amenu enable PopUp.Definition
+            amenu enable PopUp.References
+        ]]
+        end
+
+        -- local urls = require("vim.ui").get_urls()
+        -- if vim.startswith(urls[1], "http") then
+        --     vim.cmd [[
+        --         amenu enable PopUp.URL
+        --     ]]
+        -- end
+    end
+})
