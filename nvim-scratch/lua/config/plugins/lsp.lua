e3838c4e698ee36d1d4c5cd7fd2460b5a8d876c9
Pawel Lipinski <lipinskijob@gmail.com>
2024-12-30 10:15:23 +0100

    [nvim-scratch] Add blink plugin

--- nvim-scratch/lua/config/plugins/lsp.lua
+++ nvim-scratch/lua/config/plugins/lsp.lua
@@ -2,18 +2,23 @@ return {
     'neovim/nvim-lspconfig',
     enabled = true,
     dependencies = {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        'saghen/blink.cmp',
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
             },
         },
     },
     config = function()
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
         require("lspconfig").lua_ls.setup {
+            capabilities = capabilities,
             cmd = { "lua_ls" },
         }
 
