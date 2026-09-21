5dbfad0026ddcff1d0604f07ad5d6637e3affcf5
Pawel Lipinski <lipinskijob@gmail.com>
2026-09-21 15:28:43 +0200

    [system] Update docker install script

--- system/install-tools.sh
+++ system/install-tools.sh
@@ -5,26 +5,32 @@ echo "Installing tools"
 docker() {
   echo "Install docker"
   sudo apt update
-  sudo apt install -y ca-certificates curl gnupg lsb-release
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt install -y ca-certificates curl
 
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+      -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+  # Install Docker
   sudo apt update
-  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
+  # Start Docker
+  sudo systemctl enable --now docker
 
-  echo "Start docker on boot time"
-  echo "sudo systemctl enable docker.service"
-  echo "sudo systemctl enable containerd.service"
-
-  echo "To uninstall docker engine"
-  echo "sudo apt purge docker-ce docker-ce-cli containerd.io docker-compose-plugin"
-  echo "sudo rm -rf /var/lib/docker"
-  echo "sudo rm -rf /var/lib/containerd"
+  # Allow current user to use Docker without sudo
+  sudo groupadd -f docker
+  sudo usermod -aG docker "$USER"
 }
 
 java() {
